38df752ba6d919332f9a20961f263cc68c405991
joel <devnull@localhost>
2013-02-09 15:34:47 -0800

    Common enum file header

---
+++ core/types.h
@@ -0,0 +1,15 @@
+/* Define enumerations */
+
+#ifndef __TYPES__
+#define __TYPES__
+
+//namespace colors
+//{
+    enum colors {DEFAULT, RED, BLUE, YELLOW, GREEN};
+//}
+//namespace images
+//{
+    enum images {UNKNOWN, SWORD, TRIDENT, SHIELD, NET};
+//}
+
+#endif
