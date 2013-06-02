b4342a6136dfe7a7792b6a6acf47c1d4b86a58ba
Logan Evans <loganpevans@gmail.com>
2013-06-02 01:08:46 -0700

    Added some vision tests for the TollboothDetector.

--- AIFunctionCall/vision/detect_hexagon.cpp
+++ AIFunctionCall/vision/detect_hexagon.cpp
@@ -1,7 +1,27 @@
+#include "./detect_hexagon.h"
+
+namespace vision {
+  TollboothDetector::TollboothDetector(string filename) {
+    m_frame = imread(filename);
+  }
+
+  TollboothDetector::TollboothDetector(Mat frame) {
+    m_frame = frame;
+  }
+
+  TollboothDetector::~TollboothDetector() {
+    m_frame.release();
+  }
+
+  // TODO: Implement
+  bool TollboothDetector::can_see_tollbooth() {
+    return false;
+  }
+}
+
 // This is a throwaway stub used to
 // confirm that gtest works.
 int testing_gtest() {
   return 1337;
 }
-
 
