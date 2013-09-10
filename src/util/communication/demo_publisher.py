a32edc86e89c5e2494069d5c457498c62078b3b5
Logan Evans <logan.evans@isilon.com>
2013-09-10 16:42:34 -0700

    Fixed the demo zeromq code to use a non-blocking approach.

--- src/util/communication/demo_publisher.py
+++ src/util/communication/demo_publisher.py
@@ -1,11 +1,15 @@
+# COPYRIGHT: Robosub Club of the Palouse under the GPL v3
 import time
 import zmq
 from random import random
+
+UPDATE_FREQUENCY = 0.3
 
 def main():
     """ main method """
     message = {
         'module': None,
+        'message_number': None,
         'time': None,
         'move': {
             'x': None,
@@ -21,8 +25,11 @@ def main():
     publisher = context.socket(zmq.PUB)
     publisher.bind("tcp://*:5563")
     message['module'] = 'util/communication/demo_publisher'
+    message_number = 1
 
-    for _ in range(5):
+    while True:
+        message_number += 1
+        message['message_number'] = message_number
         message['move']['x'] = random() * 2 - 1
         message['move']['y'] = random() * 2 - 1
         message['move']['z'] = random() * 2 - 1
@@ -32,7 +39,8 @@ def main():
         message['time'] = time.time()
 
         publisher.send_json(message)
-        time.sleep(1)
+        print "Sent message {0}.".format(message_number)
+        time.sleep(UPDATE_FREQUENCY)
 
     publisher.close()
     context.term()
--- src/util/communication/demo_subscriber.py
+++ src/util/communication/demo_subscriber.py
@@ -1,20 +1,28 @@
+# COPYRIGHT: Robosub Club of the Palouse under the GPL v3
 import zmq
 import time
 
+REFRESH_FREQUENCY = 1
+
 def main():
     """ main method """
-
     # Prepare our context and publisher
-    context    = zmq.Context(1)
+    context = zmq.Context(1)
     subscriber = context.socket(zmq.SUB)
     subscriber.connect("tcp://localhost:5563")
     subscriber.setsockopt(zmq.SUBSCRIBE, '')
 
-    start = time.time()
-    # TODO: This timer thing doesn't work. Use threads.
-    while time.time() - start < 10:
-        message = subscriber.recv_json()
-        print(message)
+    while True:
+        # Process all messages in the queue
+        while True:
+            try:
+                # Don't block while waiting for a message.
+                message = subscriber.recv_json(zmq.DONTWAIT)
+                print(message)
+            except zmq.core.error.ZMQError:
+                print "No message in queue."
+                break
+        time.sleep(REFRESH_FREQUENCY)
 
     subscriber.close()
     context.term()
@@ -22,3 +30,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
