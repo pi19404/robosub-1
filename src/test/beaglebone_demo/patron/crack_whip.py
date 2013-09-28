28b60d45f5bfb91e6c0dd65d574d2745f33b1cb2
LoganEvans <loganpevans@gmail.com>
2013-09-28 03:09:50 +0000

    Created demo code for the beaglebone.

---
+++ src/test/beaglebone_demo/patron/crack_whip.py
@@ -0,0 +1,22 @@
+# COPYRIGHT: Robosub Club of the Palouse under the GPL v3
+
+import time
+import sys
+from random import random
+sys.path.append("../../..")  # Place src on path
+import util.communication.grapevine as grapevine
+
+def main():
+    com = grapevine.Communicator(
+            module_name='test/beaglebone_demo/patron',
+            comm_json_path="../demo_config.json")
+
+    i = 0
+    while True:
+        i += 1
+        com.publish_message({'lights': i})
+        time.sleep(0.05)  # Any faster and we can't see the bottom light.
+
+if __name__ == '__main__':
+    main()
+
