0379e35e9bf1d72deaa2f90842e5e5292ae8a9ce
Cameron Evans <c.d.evans87@gmail.com>
2013-10-17 20:39:23 -0700

    Updated video_logger.py.

--- src/sensor/vision/logging/video_logger.py
+++ src/sensor/vision/logging/video_logger.py
@@ -1,18 +1,24 @@
+#!/usr/bin/env python
 import cv2
 import cv2.cv as cv
 
 
 class VideoLogger(object):
 	def __init__(self):
-		self.Filename = "video_output"
+		self.Filename = "video_output.avi"
 		self.Fourcc = cv.CV_FOURCC('M', 'J', 'P', 'G')
 		self.Fps = 10
 		self.Width = 640
 		self.Height = 480
-		self.FrameSize = (Width, Height)
-		self.videoWriter = cv2.VideoWriter().open(Filename = filename, fourcc = Fourcc, fps = Fps, frameSize = FrameSize)
-		self.image = cv2.VideoCapture()
+		self.FrameSize = (self.Width, self.Height)
+		self.videoWriter = cv2.VideoWriter()
+		self.videoWriter.open(filename = self.Filename, 
+				      fourcc = self.Fourcc, 
+				      fps = self.Fps, 
+				      frameSize = self.FrameSize)
 		self.camera = 0
+		self.cap = cv2.VideoCapture(self.camera)
+		self.image = None
 
 		# status is a variable that can be used for error passing
 		self.status = 0
@@ -20,7 +26,7 @@ class VideoLogger(object):
 	# Grabs an image from the camera
 	def RecieveImage(self):
 		# TODO: set the status variable depending on outcome of attempting to grab an image.
-		self.image = cv2.VideoCapture(self.camera)
+		_, self.image = self.cap.read()
 
 	# Writes the current image to file (the filename is set in __init__():)
 	def WriteImage(self):
@@ -32,3 +38,7 @@ if __name__ == '__main__':
 	while(1):
 		videoLogger.RecieveImage()
 		videoLogger.WriteImage()
+		cv2.imshow('video test', videoLogger.image)
+		key = cv2.waitKey(10)
+		if key == 27:
+			break	
