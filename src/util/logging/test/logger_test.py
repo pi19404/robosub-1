26b76ce4e915b9b67affa8737b641f0c40ffc751
Dustin Crossman <dustin.crossman@gmail.com>
2013-10-22 01:00:56 -0700

    Logger module should now output a .log file for every module it's listening to

--- src/util/logging/test/logger_test.py
+++ src/util/logging/test/logger_test.py
@@ -1,4 +1,4 @@
-# COPYRIGHT: Robosub Club of the Palouse under the GPL v3
+#COPYRIGHT: Robosub Club of the Palouse under the GPL v3
 
 """
 
@@ -7,80 +7,78 @@ Logger
 """
 """
 Plan:
-	-Create communicator object for each sensor? Or just subscribe? (example: com = Communicator (module_name, mode) (use comm_json_path as testing utility)
-		-Looks like create communicator object then subscribe
-	-Use com.get_messages () or maybe [module].get_last_message
-	-Checkout logger module for python (CMehl's suggestion)
-	-Nothing will subscribe to this right?
-	
-	Sensors:
-		Motors?
-		Accelerometer
-		Hydrophones?
-		Gyroscope
-		Batteries
-		Depth Sensor
-		Internal Pressure Sensor
-		Internal Temperature Sensor
+   -Create communicator object for each sensor? Or just subscribe? (example: com = Communicator (module_name, mode) (use comm_json_path as testing utility)
+	   -Looks like create communicator object then subscribe
+   -Use com.get_messages () or maybe [module].get_last_message
+   -Checkout logger module for python (CMehl's suggestion)
+   -Nothing will subscribe to this right?
+
+   Sensors:
+	   Motors?
+	   Accelerometer
+	   Hydrophones?
+	   Gyroscope
+	   Batteries
+	   Depth Sensor
+	   Internal Pressure Sensor
+	   Internal Temperature Sensor
 
 """
 
 
-import time
+from time import sleep
 import os
 import sys
-sys.path[0] = "/home/dustin/robosub/src/util/communication/"
-#sys.path.append(os.path.abspath(".."))
-#sys.path.append(os.path.abspath("/communication/"))
-import grapevine
-sys.path[0] = "/home/dustin/robosub/src/util/logging/test/"
+sys.path.append(os.path.abspath("../../../util/communication"))
+#from util.communication.grapevine import Communicator #preffered method
+from grapevine import Communicator
 
-UPDATE_FREQUENCY = 3
+UPDATE_FREQUENCY= 3
 
 
 def main ():
 
-	"""
-	Logger = grapevine.Communicator (
-		module_name = 'util/logging/logger',
-		mode = "debug",
-		comm_json_path = "./mock_logger_settings.json")
-		
-	print Logger.get_message('util/communication/test/mock_log')
-	"""
-
-	#sys.path[0] = "/home/dustin/robosub/src/util/communication"
-	#sys.path[0] = "/home/dustin/robosub/src/"
-	com = grapevine.Communicator(
-			module_name='util/logging/test/logger',
+	com = Communicator(
+		module_name='util/logging/test/logger',
 			comm_json_path="./mock_logger_settings.json")
 
+##################################################
+# This code outputs a file for each sensor       #
+##################################################
+
+	i = 0
 	while True:
-		print com.get_last_message('util/logging/test/mock_log')
-		"""
-		i = 0
-		for msg in com.get_messages('util/communication/test/mock_log'):
+		for sensor in com.listening(): #is constantly querying this an efficient way of doing this?
+			#filename = str (str (sensor).replace ('/', '-') + '.log') #replace() is needed because '/'s are not allowed in file names
+			filename = str (str (sensor [sensor.rfind ('/') + 1:]) + '.log') #this strips out everything before and including the last '/'
+			with open (filename, 'a') as sensor_out: #is constantly opening and reopening the file an efficient way of doing this?
+				sensor_out.write (str (com.get_last_message (sensor)) + '\n')
+			print 'Recieved', i, 'messages for each module'
 			i += 1
-			print msg
-		print i, 'messages received'
-		#com.publish_message("can you hear me now?")
-		"""
-		time.sleep(UPDATE_FREQUENCY)
+		sleep (UPDATE_FREQUENCY)
 		
-		
-		
-	
-		
-		
+###################################################
+# This code outputs a single file for all sensors #
+###################################################
 """
-	logger = open (logging.txt, 'w')
+	logger = open ("logging.log", 'w')
 
-	for module in com.listening ():
-		logger.write (com.get_last_message (module))
+	while True:
+		for module in com.listening():
+			message = com.get_last_message (module)
+			print message
+			logger.write (str (message) + '\n')
+		logger.write ('\n')
+		sleep (UPDATE_FREQUENCY)
+
+	logger.close()
 """
-		
-		
 
 
-if __name__ == '__main__':
-    main()
+
+
+
+
+
+if __name__ == "__main__":
+	main()
