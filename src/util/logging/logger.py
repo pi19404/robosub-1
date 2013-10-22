bbdd7420e6d93a8fd9f51f1d2b22bacbdf51eddf
Dustin Crossman <dustin.crossman@gmail.com>
2013-10-22 01:12:27 -0700

    Updated final logger module

--- src/util/logging/logger.py
+++ src/util/logging/logger.py
@@ -1,54 +1,57 @@
-# COPYRIGHT: Robosub Club of the Palouse under the GPL v3
+#COPYRIGHT: Robosub Club of the Palouse under the GPL v3
 
 """
 
 Logger
 
 """
-"""
-Plan:
-	-Create communicator object for each sensor? Or just subscribe? (example: com = Communicator (module_name, mode) (use comm_json_path as testing utility)
-		-Looks like create communicator object then subscribe
-	-Use com.get_messages () or maybe [module].get_last_message
-	-Checkout logger module for python (CMehl's suggestion)
-	-Nothing will subscribe to this right?
-	
-	Sensors:
-		Motors?
-		Accelerometer
-		Hydrophones?
-		Gyroscope
-		Batteries
-		Depth Sensor
-		Internal Pressure Sensor
-		Internal Temperature Sensor
 
-"""
-
-
-import time
+from time import sleep
 import os
 import sys
-#sys.path.append(os.path.abspath(".."))
-#sys.path.append(os.path.abspath("/communication/"))
-from util.communication.grapevine import grapevine
+sys.path.append(os.path.abspath("../../util/communication"))
+#from util.communication.grapevine import Communicator #preffered method
 from grapevine import Communicator
 
-#UPDATE_FREQUENCY = 3
-
+UPDATE_FREQUENCY= 1
 
 def main ():
-	com = grapevine.Communicator(
-			module_name='util/logging/test/logger',
-			comm_json_path="./mock_logger_settings.json")
 
-	logger = open (logging.txt, 'w')
+	com = Communicator(
+		module_name='util/logging',
+			comm_json_path="./communication_settings.json")
 
-	for module in com.listening ():
-		logger.write (com.get_last_message (module))
+##################################################
+# This code outputs a file for each sensor       #
+##################################################
+
+	i = 0
+	while True:
+		for sensor in com.listening(): #is constantly querying this an efficient way of doing this?
+			#filename = str (str (sensor).replace ('/', '-') + '.log') #replace() is needed because '/'s are not allowed in file names
+			filename = str (str (sensor [sensor.rfind ('/') + 1:]) + '.log') #this strips out everything before and including the last '/'
+			with open (filename, 'a') as sensor_out: #is constantly opening and reopening the file an efficient way of doing this?
+				sensor_out.write (str (com.get_last_message (sensor)) + '\n')
+			print 'Recieved', i, 'messages for each module'
+			i += 1
+		sleep (UPDATE_FREQUENCY)
 		
-		
+###################################################
+# This code outputs a single file for all sensors #
+###################################################
+"""
+	logger = open ("logging.log", 'w')
 
+	while True:
+		for module in com.listening():
+			message = com.get_last_message (module)
+			print message
+			logger.write (str (message) + '\n')
+		logger.write ('\n')
+		sleep (UPDATE_FREQUENCY)
 
-if __name__ == '__main__':
-    main()
+	logger.close()
+"""
+
+if __name__ == "__main__":
+	main()
