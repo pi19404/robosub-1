0afdb3522b2f96802cf80bc364fcd220298fa17d
Dustin Crossman <dustin.crossman@gmail.com>
2013-10-23 13:07:40 -0700

    Updated logger to be more efficient

--- src/util/logging/logger.py
+++ src/util/logging/logger.py
@@ -1,41 +1,95 @@
 #COPYRIGHT: Robosub Club of the Palouse under the GPL v3
 
 """
-
-Logger
-
+Logger Module
+	This listens to the various sensors and modules and outputs a log for each
 """
 
+"""
+	Sensors logger listens to (can be found in src/communication_settings.json):
+		Decision Module
+		Directive Module
+		Stabilization Module
+		Physical Module
+		Vision Module
+		Hydrophone
+		Depth Sensor
+		Accelerometer
+		Speed Sensor
+"""
+
+
 from time import sleep
+from time import time
 import os
 import sys
-sys.path.append(os.path.abspath("../../util/communication"))
-#from util.communication.grapevine import Communicator #preffered method
+#sys.path.append(os.path.abspath("../../util/communication"))
+from util.communication.grapevine import Communicator #preffered method
 from grapevine import Communicator
 
-UPDATE_FREQUENCY= 1
+UPDATE_FREQUENCY= 3
+
 
 def main ():
 
 	com = Communicator(
-		module_name='util/logging',
-			comm_json_path="./communication_settings.json")
+		module_name='util/logger',
+			comm_json_path="../../communication_settings.json")
+
+#####################################################################
+# This is a more efficient way of outputting a file for each sensor #
+#####################################################################
+
+	log_files = {} 
+	for sensor in com.listening(): #populates a dict with the module and its associated .log file
+		log_files[sensor] = open (str (str (sensor [sensor.rfind ('/') + 1:]) + '.log'), 'w') #this strips out everything before and including the last '/'
+
+	i = 0
+	while True:
+		for sensor_out in log_files:
+			message = com.get_last_message (sensor_out)
+			if not message: #will output sensor off if logger updates faster than the sensor
+				log_files[sensor_out].write ("Sensor off at time: " + str (time() ) )
+			else:
+				for k in message.keys():
+					log_files[sensor_out].write (str (k) + ':' + str (message[k]) + ' ',) #makes the .log file a bit more readable
+			log_files[sensor_out].write ('\n')
+
+		print 'Checked for messages ' + str (i) + ' times'
+		i += 1
+		sleep (UPDATE_FREQUENCY)
+
+	for sensor_file in log_files:
+		log_files[sensor_file].close()
 
 ##################################################
 # This code outputs a file for each sensor       #
 ##################################################
 
+"""
 	i = 0
 	while True:
 		for sensor in com.listening(): #is constantly querying this an efficient way of doing this?
 			#filename = str (str (sensor).replace ('/', '-') + '.log') #replace() is needed because '/'s are not allowed in file names
 			filename = str (str (sensor [sensor.rfind ('/') + 1:]) + '.log') #this strips out everything before and including the last '/'
+
 			with open (filename, 'a') as sensor_out: #is constantly opening and reopening the file an efficient way of doing this?
-				sensor_out.write (str (com.get_last_message (sensor)) + '\n')
-			print 'Recieved', i, 'messages for each module'
-			i += 1
+				#sensor_out.write (str (com.get_last_message (sensor)) + '\n')
+				message = com.get_last_message (sensor)
+
+				if not message: #will output sensor off if logger updates faster than the sensor
+					sensor_out.write ("Sensor off at time: " + str (time() ) )
+				else:
+					for k in message.keys():
+						sensor_out.write (str (k) + ':' + str (message[k]) + ' ',) #makes the .log file a bit more readable
+
+				sensor_out.write ('\n')
+
+		print 'Checked for messages' + str (i) + 'times'
+		i += 1
 		sleep (UPDATE_FREQUENCY)
-		
+"""	
+	
 ###################################################
 # This code outputs a single file for all sensors #
 ###################################################
@@ -45,13 +99,23 @@ def main ():
 	while True:
 		for module in com.listening():
 			message = com.get_last_message (module)
-			print message
+			#print message
 			logger.write (str (message) + '\n')
 		logger.write ('\n')
 		sleep (UPDATE_FREQUENCY)
 
 	logger.close()
 """
+
+###################################################
+
+
+def shorten_filename (sensor):
+	return str (str (sensor [sensor.rfind ('/') + 1:]) + '.log') #this strips out everything before and including the last '/' and stores it in a list
+
+
+
+
 
 if __name__ == "__main__":
 	main()
--- src/util/logging/test/logger_test.py
+++ src/util/logging/test/logger_test.py
@@ -1,32 +1,26 @@
 #COPYRIGHT: Robosub Club of the Palouse under the GPL v3
 
 """
-
-Logger
+Logger Module
+	This listens to the various sensors and modules and outputs a log for each
+"""
 
 """
-"""
-Plan:
-   -Create communicator object for each sensor? Or just subscribe? (example: com = Communicator (module_name, mode) (use comm_json_path as testing utility)
-	   -Looks like create communicator object then subscribe
-   -Use com.get_messages () or maybe [module].get_last_message
-   -Checkout logger module for python (CMehl's suggestion)
-   -Nothing will subscribe to this right?
-
-   Sensors:
-	   Motors?
-	   Accelerometer
-	   Hydrophones?
-	   Gyroscope
-	   Batteries
-	   Depth Sensor
-	   Internal Pressure Sensor
-	   Internal Temperature Sensor
-
+	Sensors logger listens to (can be found in src/communication_settings.json):
+		Decision Module
+		Directive Module
+		Stabilization Module
+		Physical Module
+		Vision Module
+		Hydrophone
+		Depth Sensor
+		Accelerometer
+		Speed Sensor
 """
 
 
 from time import sleep
+from time import time
 import os
 import sys
 sys.path.append(os.path.abspath("../../../util/communication"))
@@ -42,21 +36,60 @@ def main ():
 		module_name='util/logging/test/logger',
 			comm_json_path="./mock_logger_settings.json")
 
+#####################################################################
+# This is a more efficient way of outputting a file for each sensor #
+#####################################################################
+
+	log_files = {} 
+	for sensor in com.listening(): #populates a dict with the module and its associated .log file
+		log_files[sensor] = open (str (str (sensor [sensor.rfind ('/') + 1:]) + '.log'), 'w') #this strips out everything before and including the last '/'
+
+	i = 0
+	while True:
+		for sensor_out in log_files:
+			message = com.get_last_message (sensor_out)
+			if not message: #will output sensor off if logger updates faster than the sensor
+				log_files[sensor_out].write ("Sensor off at time: " + str (time() ) )
+			else:
+				for k in message.keys():
+					log_files[sensor_out].write (str (k) + ':' + str (message[k]) + ' ',) #makes the .log file a bit more readable
+			log_files[sensor_out].write ('\n')
+
+		print 'Checked for messages ' + str (i) + ' times'
+		i += 1
+		sleep (UPDATE_FREQUENCY)
+
+	for sensor_file in log_files:
+		log_files[sensor_file].close()
+
 ##################################################
 # This code outputs a file for each sensor       #
 ##################################################
 
+"""
 	i = 0
 	while True:
 		for sensor in com.listening(): #is constantly querying this an efficient way of doing this?
 			#filename = str (str (sensor).replace ('/', '-') + '.log') #replace() is needed because '/'s are not allowed in file names
 			filename = str (str (sensor [sensor.rfind ('/') + 1:]) + '.log') #this strips out everything before and including the last '/'
+
 			with open (filename, 'a') as sensor_out: #is constantly opening and reopening the file an efficient way of doing this?
-				sensor_out.write (str (com.get_last_message (sensor)) + '\n')
-			print 'Recieved', i, 'messages for each module'
-			i += 1
+				#sensor_out.write (str (com.get_last_message (sensor)) + '\n')
+				message = com.get_last_message (sensor)
+
+				if not message: #will output sensor off if logger updates faster than the sensor
+					sensor_out.write ("Sensor off at time: " + str (time() ) )
+				else:
+					for k in message.keys():
+						sensor_out.write (str (k) + ':' + str (message[k]) + ' ',) #makes the .log file a bit more readable
+
+				sensor_out.write ('\n')
+
+		print 'Checked for messages' + str (i) + 'times'
+		i += 1
 		sleep (UPDATE_FREQUENCY)
-		
+"""	
+	
 ###################################################
 # This code outputs a single file for all sensors #
 ###################################################
@@ -66,7 +99,7 @@ def main ():
 	while True:
 		for module in com.listening():
 			message = com.get_last_message (module)
-			print message
+			#print message
 			logger.write (str (message) + '\n')
 		logger.write ('\n')
 		sleep (UPDATE_FREQUENCY)
@@ -74,7 +107,11 @@ def main ():
 	logger.close()
 """
 
+###################################################
 
+
+def shorten_filename (sensor):
+	return str (str (sensor [sensor.rfind ('/') + 1:]) + '.log') #this strips out everything before and including the last '/' and stores it in a list
 
 
 
