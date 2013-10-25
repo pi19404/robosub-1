2c9bd1701a12e5b4b8ff113cf1c80c5269eeaae6
DanielRetherford <danielretherford@gmail.com>
2013-10-24 19:41:24 -0700

    User Input Movement
    This is the baseline code for the user input control of the robo sub.

---
+++ src/movement/directive/UserInputControl.py
@@ -0,0 +1,80 @@
+#Daniel Retherford
+#RoboSub Movement Program
+
+#Imported Libraries
+from msvcrt import getch
+import time
+
+
+#Functions
+#Controls the depth of the robo sub
+def userChangeDepth(keyPress):
+    x = 0
+    speed = 2.0
+    speedAdjustment = 2.3
+    if keyPress == "down":
+        #Move sub down
+        for x in range(0, 3):
+            print("Sub moving down at", speed)
+            speed = speed + speedAdjustment
+            time.sleep(1)
+    elif keyPress == "up":
+        #Move sub up
+        for x in range(0, 3):
+            print("Sub moving up at", speed)
+            speed = speed + speedAdjustment
+            time.sleep(1)
+    
+    speed = 2.0 #set back to original value
+    return
+
+#Controls the forward and backwards movement of the sub
+def userForwardBackwardChange(keyPress):
+    if keyPress == "forward":
+        print("Sub is moving forward at")
+    elif keyPress == "backwards":
+        print("Sub is moving backwards at")
+    return
+
+#Controls the rotational movement left and right of the sub
+def userRotationChange(keyPress):
+    if keyPress == "left":
+        print("Sub is rotating to the left at")
+    elif keyPress == "right":
+        print("Sub is rotating to the right at")
+
+def main():
+    while True:
+        #Used to capture the keypress
+        key = ord(getch())
+        #224 is the dafault special key for the keyboard
+        if key == 224: #Special keys (arrows, f keys, ins, del, etc.)
+            key = ord(getch())
+
+            #Mini Code block bellow controls the up and down
+            if key == 81: #Down arrow
+                userChangeDepth("down")
+            elif key == 73: #Up arrow
+                userChangeDepth("up")
+            
+
+            #Code block below is code to control direction "left, right, forward, backward"
+            #Code is to rotate left and right    
+            elif key == 75: # left arrow
+                userRotationChange("left")
+            elif key == 77: # right arrow
+                userRotationChange("right")
+                
+            #Code is to move forward and backward
+            elif key == 72: # up arrow
+                userForwardBackwardChange("forward")
+            elif key == 80: # down arrow
+                userForwardBackwardChange("backwards")
+
+            #Code to shut down all moters
+            elif key == 27:
+                print("motors are shutting off")
+                
+if __name__ == "__main__":
+    main()
+        
