48a604149b192e94b9437737e9d432def04a0a3e
cevans87 <c.d.evans87@gmail.com>
2014-02-08 10:41:55 -0800

    Forgot to add files last commit.

---
+++ src/sensor/vision/plugins/Streamer.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import os
+import cv2
+import cv2.cv as cv
+import zmq
+#import threading
+#from multiprocessing.pool import ThreadPool
+from time import strftime
+
+#FIXME this logger should be JUST A LOGGER. Video grabbing should happen
+#elsewhere.
+
+class Streamer(object):
+
+    def __init__(self, fp, settings):
+        """Initialize video stream server.
+
+        Args:
+
+        """
+        self._fp = fp
+        self._settings = settings
+        self._stream = {}
+        self._stream['context'] = zmq.Context(1)
+        self._stream['socket'] = \
+                    self._stream['context'].socket(zmq.PAIR)
+        self._stream['socket'].bind(
+                    "tcp://*:{port}".format(port=self._settings['stream_port']))
+
+    def process_image(self, packet):
+        metadata = dict(dtype = str(self._fp.im.dtype), shape = self._fp.im.shape)
+        try:
+            self._stream['socket'].send_json(
+                    metadata, flags=zmq.SNDMORE | zmq.NOBLOCK)
+            self._stream['socket'].send(
+                    self._fp.im, copy=True, track=False, flags=zmq.NOBLOCK)
+        except zmq.ZMQError:
+            pass
+
---
+++ src/util/monitoring/crystal_ball2.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import zmq
+import time
+import json
+import numpy
+import cv2
+import cv2.cv as cv
+import time
+from time import sleep
+sys.path.append(os.path.abspath("../.."))
+from util.communication.grapevine import Communicator
+
+class VisionViewer(object):
+    def __init__(self):
+        self.com = Communicator(
+                module_name="util/vision_viewer")
+               # subscriber_buffer_length=81920,
+               # subscriber_high_water_mark=81920)
+        last_timestamp = 0.0
+        #self.com.connect_video_stream(50000, '192.168.1.100')
+        self.com.connect_video_stream(50001)
+        print 'connected'
+        while True:
+            print 'hmm'
+            try:
+                im = self.com.recv_image()
+            except ValueError:
+                sleep(.1)
+                im = None
+            if im is not None:
+                print 'got an image'
+                cv2.imshow('test window', im)
+                key = cv2.waitKey(1)
+
+if __name__ == '__main__':
+    viewer = VisionViewer()
