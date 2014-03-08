71cf3bb9e8dc070483e5f7fc0bcef5b24dbb1111
Dustin Crossman <dustin.crossman@gmail.com>
2014-03-07 19:42:48 -0800

    Revert "Removed local microcontroller branch."
    This reverts commit 1479e5ffa7d19ac15a227383ffaf8af79caf87ea.

---
+++ src/microcontroller/Timer_ISR.c
@@ -0,0 +1,91 @@
+/********************************************************
+ *   File Name: Timer_ISR.c
+ *
+ *   Description:
+ *              source code file for the Timer ISR
+ *
+ *
+ *********************************************************/
+#include "functions.h"
+#include "Sensors.h"
+#include "Timer_ISR.h"
+#include "peripheral/timer.h"
+#include "I2C_ISR.h"
+#include "comm_UART_ISR.h"
+#include "motor_UART_ISR.h"
+#include "system.h"
+#include "ADC_ISR.h"
+
+
+/*************************************************************************
+ Variables
+ ************************************************************************/
+
+/********************************************************
+ *   Function Name:
+ *
+ *   Description:
+ *
+ *
+ *********************************************************/
+void timer_1_setup(void)
+{
+    OpenTimer1(T1_ON | T1_SOURCE_INT | T1_PS_1_256, 0x1ff0);
+
+    IPC1bits.T1IP = 7;
+    IEC0bits.T1IE = 1;
+}
+
+/********************************************************
+ *   Function Name:
+ *
+ *   Description:
+ *
+ *
+ *********************************************************/
+inline void timer_1_begin(void)
+{
+    IFS0bits.T1IF = 1; //set the interrupt flag
+}
+
+/********************************************************
+ *   Function Name:
+ *
+ *   Description:
+ *
+ *
+ *********************************************************/
+void __ISR(_TIMER_1_VECTOR, IPL7AUTO) Timer1Handler(void)
+{
+    extern boolean I2C_BANK_0_is_idle;
+    extern boolean COMM_UART_is_idle;
+    extern boolean ADC_is_idle;
+    INTDisableInterrupts();
+
+    //PORTCbits.RC1 = !PORTCbits.RC1; //toggle LED5 (max32)
+    i2c_GYRO_Read();
+    i2c_ACL_Read();
+
+    
+        comm_uart_CreateNode( 'A', 'B', 'C' );
+        if (COMM_UART_is_idle)
+        {
+            comm_uart_begin();
+        }
+    
+    if (I2C_BANK_0_is_idle)
+    {
+        //PORTAbits.RA3 = !PORTAbits.RA3; //toggle LED4 (max32)
+        i2c_bank_0_begin();
+    }
+
+    ADC_Depth_Read();
+    ADC_Battery_Read();
+    if (ADC_is_idle)
+    {
+        adc_begin();
+    }
+        
+    IFS0bits.T1IF = 0; //clear the interrupt flag
+    INTEnableInterrupts();
+}
---
+++ src/microcontroller/motor_UART_ISR.h
@@ -0,0 +1,140 @@
+/*
+ * File:   motor_UART_ISR.h
+ * Author: Korey
+ *
+ * Created on September 27, 2013, 10:34 AM
+ */
+
+#ifndef MOTOR_UART_ISR_H
+#define	MOTOR_UART_ISR_H
+
+/*************************************************************************
+ System Includes
+ ************************************************************************/
+#include <peripheral/ports.h>
+#include <peripheral/uart.h>
+
+/*************************************************************************
+ Typedefs
+ ************************************************************************/
+
+/*************************************************************************
+ Constants
+ ************************************************************************/
+#define MOTOR_UARTQueueSize 32
+/*************************************************************************
+ Structure Definitions
+ ************************************************************************/
+typedef struct motor_uart_node
+{
+    uint8 uart_data[4];
+//    uint8 sensor_id;
+//    uint8 sensor_sub_id;
+//    uint8 data_h;
+//    uint8 data_l;
+}MOTOR_UART_NODE;
+
+typedef struct motor_uart_queue
+{
+    MOTOR_UART_NODE DataBank[MOTOR_UARTQueueSize]; //Array of Nodes of the #defined size QueueSize
+    uint8 QueueStart; //Location of oldest node
+    uint8 QueueEnd; //Location of next free slot
+    uint8 QueueLength; //Length of queue
+}MOTOR_UART_QUEUE;
+
+/*************************************************************************
+ Enums
+ ************************************************************************/
+
+/*************************************************************************
+ Variables
+ ************************************************************************/
+
+/*************************************************************************
+ Function Declarations
+ ************************************************************************/
+
+ /********************************************************
+ *   Function Name:
+ *
+ *   Description:
+ *
+ *
+ *********************************************************/
+void motor_uart_setup(void);
+void motor_uart_begin(void);
+
+/********************************************************
+ *   Function Name: motor_uart_InitializeQueue(MOTOR_UART_QUEUE* queue)
+ *
+ *   Description: Clears the queue and resets parameters
+ *
+ *
+ *********************************************************/
+void motor_uart_InitializeQueue(MOTOR_UART_QUEUE* queue);
+
+/********************************************************
+ *   Function Name: motor_uart_addToQueue(MOTOR_UART_QUEUE* queue, MOTOR_UART_NODE new_node)
+ *
+ *   Description: Adds a node to the queue - Pass a node by reference
+ *
+ *
+ *********************************************************/
+int motor_uart_addToQueue(MOTOR_UART_QUEUE* queue, MOTOR_UART_NODE new_node);
+
+/********************************************************
+ *   Function Name: motor_uart_popNode(MOTOR_UART_QUEUE* queue, MOTOR_UART_NODE* return_node)
+ *
+ *   Description: Pulls the next node off the queue
+ *
+ *
+ *********************************************************/
+int motor_uart_popNode(MOTOR_UART_QUEUE* queue, MOTOR_UART_NODE* return_node);
+
+ /********************************************************
+ *   Function Name: motor_uart_SetNode( uint Byte1, uint Byte2, uint Byte3, uint Byte4 )
+ *
+ *   Description: Creates a node using three bytes
+ *
+ *
+ *********************************************************/
+void motor_uart_CreateNode( uint Byte1, uint Byte2, uint Byte3, uint Byte4 );
+
+ /********************************************************
+ *   Function Name: Motor1_Forward( uint address, uint speed )
+ *
+ *   Description: Create a node to move motor 1 forward
+ *
+ *
+ *********************************************************/
+void Motor1_Forward( uint address, uint speed );
+
+ /********************************************************
+ *   Function Name: Motor1_Backward( uint address, uint speed )
+ *
+ *   Description: Create a node to move motor 1 backward
+ *
+ *
+ *********************************************************/
+void Motor1_Backward( uint address, uint speed );
+
+ /********************************************************
+ *   Function Name: Motor1_Forward( uint address, uint speed )
+ *
+ *   Description: Create a node to move motor 2 forward
+ *
+ *
+ *********************************************************/
+void Motor2_Forward( uint address, uint speed );
+
+ /********************************************************
+ *   Function Name: Motor1_Backward( uint address, uint speed )
+ *
+ *   Description: Create a node to move motor 2 backward
+ *
+ *
+ *********************************************************/
+void Motor2_Backward( uint address, uint speed );
+
+#endif	/* MOTOR_UART_ISR_H */
+
