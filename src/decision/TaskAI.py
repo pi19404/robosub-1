3a1c105c88848dbf1f8c7c85cc36de8d82bf0a78
Chris Hyde <hyde.wsu@gmail.com>
2014-04-02 13:56:03 -0700

    moved some unused stubs to 'Legacy' folder

--- src/decision/TaskAI.py
+++ src/decision/TaskAI.py
@@ -1,1 +1,27 @@
 # Task AI 
+
+
+class AIStateMachine(AITools):
+	"""This is the home of the AI state machine controlled by the master 
+	AI.  This class instanciates sub-class AI "tasks" that run the sub
+	"""
+	def __init__()
+	
+	
+	
+"""
+Robosub Notes LOL
+
+For Sunday Pool Test
+* Start Gate
+* Line Following
+
+TONIGHT-> hammer out packets
+
+>> other peeps
+	> camera testing
+
+
+
+"""
+ 
