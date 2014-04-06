cc15008cee3da32695deaf24aa843af25fcb0b9a
Matt Hintzke <mhintzke.wsu@hotmail.com>
2014-04-05 18:30:02 -0700

    fixed basetaskai

--- src/decision/tasks/BaseTaskAI.py
+++ src/decision/tasks/BaseTaskAI.py
@@ -28,22 +28,6 @@ class BaseTaskAI():
 	def publishCommand(self, packet):
 		self.com.publish_message(packet)
 		
-	def getBlankPacket(self):
-	        return {
-	            "Task_AI_Movement": {
-	                "override":[], # override module (to hold strings
-	                "position": {
-	                    "right": 0.0,
-	                    "forward": 0.0,
-	                    "up": 0.0
-	                },
-	                "orientation": { # Notably, this is the desired state, not a requested change in state
-	                    "pitch_up": 0.0, 
-	                    "roll_right": 0.0,
-	                    "heading": 0.0  #looking down, Clockwise, in radians
-	                    }
-	                }
-	            }
 	def isStable(self, numSamples, limit):
 		# Use standard deviation on Gyroscope and/or Accelerometer
 		# to measure stability in sensor data
